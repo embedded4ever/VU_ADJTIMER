9d67a4f2cffceb6e32b5d1db420cb64efc162409
Volkan Unal <engineeringvolkan@hotmail.com>
2018-04-27 19:35:00 +0300

    Add files via upload

---
+++ Example/serialtest.ino
@@ -0,0 +1,21 @@
+#include "/VU_ADJTIMER.h"
+VU_ADJTIMER serial;
+void setup()
+{
+  Serial.begin(9600);
+  serial.set(1000,test);
+  serial.start();
+  
+}
+void loop()
+{
+ Serial.println("work with loop");
+ delay(1000);
+ 
+}
+void test()
+{
+  Serial.println("work with isr");
+}
+
+
